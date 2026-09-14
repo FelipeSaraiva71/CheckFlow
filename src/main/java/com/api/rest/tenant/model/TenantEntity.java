76dfa3f28a60f531e54d0f07bd8d391792c1b71d
Felipe Saraiva <felipeluizsaraiva71@gmail.com>
2026-09-14 17:09:22 -0300

    Feito anotações entity tenant.

--- src/main/java/com/api/rest/tenant/model/TenantEntity.java
+++ src/main/java/com/api/rest/tenant/model/TenantEntity.java
@@ -13,8 +13,10 @@ public class TenantEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, length = 200)
     private String nome;
 
+    @Column(nullable = false, length = 10)
     private StatusTenantEnum status;
 
 }
